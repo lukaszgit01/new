e78372b718fa1d171e4be19c4629344446766cb8
lukaszgit01 <lukaszweg@icloud.com>
2024-10-12 18:21:03 +0200

    wow wow megasaas

--- sadas.java
+++ sadas.java
@@ -1,5 +1,62 @@
+import java.util.Scanner;
+
 public class sadas {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+/*
+        int firstNumber = 5;
+        int secondNumber = 7;
+        int resolt = firstNumber + secondNumber;
+        System.out.println(resolt);
+*/
+       /*
+        float my_Float_number = 12.03f;
+        int my_int_number = 6;
+        double resolt = my_int_number / my_Float_number  ;
+
+        System.out.println(resolt);
+
+        */
+   /*
+        boolean boolValue1 = true;
+   boolean boolVAlue2 = false;
+   System.out.println(boolValue1 || boolVAlue2);
+
+
+    */
+       /*
+        int a = 4;
+       int b = 6;
+       boolean result = a > b ;
+
+       if (result){
+           System.out.println(result);
+            System.out.println("Liczba " + a + " jest większa od liczby " + b );
+       } else if (a == b) {
+           System.out.println("Liczby są sobie równe");
+
+       } else {
+
+           System.out.println(b + " jest większe od " + a);
+
+        */
+        /*
+        for (int i = 0; i < 12; i++) {
+            System.out.print(i + ", ");
+
+         */
+//    int i = 0;
+//    while (i < 10) {
+//        System.out.println(i + " ");
+//        ++i;
+       }
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Podaj liczbę");
+        int input_Number = scanner.nextInt();
+
+        System.out.println()
+
+
     }
+
 }
